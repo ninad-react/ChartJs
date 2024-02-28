83782dfe52571bedfd5d76682ecdc5d27d8f8d30
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-02-28 11:02:53 +0530

    chartJs demo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      
+      <div className='dataCard revenueCard'>Chart 1</div>
+
+      <div className='dataCard customerCard'>Chart 2</div>
+
+      <div className='dataCard categoryCard'>Chart 3</div>
+
+    </div>
+  )
+}
+
+export default App
